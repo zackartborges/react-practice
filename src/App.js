fac19e18dbd3a0e25edec4a7b2a05f88f0c786b7
Zack Borges <zackartborges@gmail.com>
2021-07-02 13:45:48 -0400

    gets components in order, developed understanding of components and index vs app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-// import logo from './logo.svg';
-import React from "react"
-import ReactDOM from "react-dom"
-// import './App.css';
-import MyInfo from './components/MyInfo'
+import logo from './logo.svg';
+// import React from "react"
+// import ReactDOM from "react-dom"
+import './App.css';
+// import MyInfo from './components/MyInfo'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+Welcome to My Practice App
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-ReactDOM.render(<MyInfo />, document.getElementById("root") )
-// export default ;
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MyInfo from './components/MyInfo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyInfo />
+    <App />
+    {/* <MyInfo /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
